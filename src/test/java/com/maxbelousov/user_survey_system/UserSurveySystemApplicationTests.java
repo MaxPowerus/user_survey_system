ad9237a1c975c71b810e7b1b6b57ee5800cd63a8
MaksimBelousov <Maksim_Belousov@epam.com>
2022-05-14 15:46:12 +0300

    init commit

---
+++ src/test/java/com/maxbelousov/user_survey_system/UserSurveySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxbelousov.user_survey_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSurveySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
