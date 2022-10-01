9d724644e068afe8a7d31dc8b4f5c6557936acab
MaksimBelousov <m.e.belousov@mail.ru>
2022-10-01 12:54:21 +0300

    added user entity

---
+++ src/main/java/com/maxbelousov/user_survey_system/entity/User.java
@@ -0,0 +1,25 @@
+package com.maxbelousov.user_survey_system.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "user_id")
+  private Long id;
+
+}
