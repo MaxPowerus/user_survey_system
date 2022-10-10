d2acb0dd03bb6925f2a0b8cab4b896e5394d1737
MaksimBelousov <m.e.belousov@mail.ru>
2022-10-10 14:46:41 +0300

    corrected SurveyNotFoundException

--- src/main/java/com/maxbelousov/user_survey_system/exception/SurveyNotFoundException.java
+++ src/main/java/com/maxbelousov/user_survey_system/exception/SurveyNotFoundException.java
@@ -3,7 +3,7 @@ package com.maxbelousov.user_survey_system.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class SurveyNotFoundException extends RuntimeException{
   public SurveyNotFoundException(String errorMessage){
     super(errorMessage);
